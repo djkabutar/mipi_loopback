8ecbaa54dba3b8a75703cdd06114e35798e1e046
djkabutar <d.kabutarwala@yahoo.com>
2023-09-16 11:29:16 +0530

    top: mipi core: give delay to the valid as there can be fifo in core
    In here, there could be the FIFO in the MIPI RX & TX Core given
    from the Efinix, supposing that the data will be delayed automatically
    while receiving and transmitting of a clock cycle.
    
    Therefore, data is always coming back from TX as pixels are not
    properly oriented.
    
    To remove the problem, here, valid will be delayed for 2 clock
    cycles as it is getting delayed while receiving as well as
    transmitting.
    
    Also removed, fixed comments and removed unnecessary declarations
    from the design.

--- mipi_loopback_top.v
+++ mipi_loopback_top.v
@@ -1,33 +1,19 @@
-/////////////////////////////////////////////////////////////////////////////
-//
-// Copyright (C) 2013-2019 Efinix Inc. All rights reserved.
-//
-// mipi_loopback_top.v
-//
-// *******************************
-// Revisions:
-// 1.0 Initial rev
-// *******************************
-/////////////////////////////////////////////////////////////////////////////
-
 module mipi_loopback_top (
-/* Signals of the video pattern generator */
-	input         tx_vga_clk,
-    
     output        led5,
     output        led6,
+
 /* Clocks of MIPI TX and RX parallel interfaces */
 	input         tx_pixel_clk,
 	input         rx_pixel_clk,
 /* Signals used by the MIPI TX Interface Designer instance */
-	  
+
 	output        my_mipi_tx_DPHY_RSTN,
 	output        my_mipi_tx_RSTN,
 	output        my_mipi_tx_VALID,
 	output        my_mipi_tx_HSYNC,
 	output        my_mipi_tx_VSYNC,
 	output [63:0] my_mipi_tx_DATA,
-	
+
 	output [5:0]  my_mipi_tx_TYPE,
 	output [1:0]  my_mipi_tx_LANES,
 	output        my_mipi_tx_FRAME_MODE,
@@ -38,14 +24,12 @@ module mipi_loopback_top (
 	output        my_mipi_tx_ULPS_CLK_ENTER,
 	output        my_mipi_tx_ULPS_CLK_EXIT,
 /* Signals used by the MIPI RX Interface Designer instance */
-	  
+
 	output        my_mipi_rx_DPHY_RSTN,
 	output        my_mipi_rx_RSTN,
 	output        my_mipi_rx_CLEAR,
 	output [1:0]  my_mipi_rx_LANES,
-	output [3:0]  my_mipi_rx_VC_ENA,
-    output        rx_vs,
-    output        rx_hs,
+
 	input         my_mipi_rx_VALID,
 	input [3:0]   my_mipi_rx_HSYNC,
 	input [3:0]   my_mipi_rx_VSYNC,
@@ -57,13 +41,16 @@ module mipi_loopback_top (
 	input         my_mipi_rx_ULPS_CLK,
 	input [3:0]   my_mipi_rx_ULPS
 );
+
+reg prev_2_valid = 0;
+reg prev_valid = 0;
                    
-assign my_mipi_tx_VALID = my_mipi_rx_VALID;
+assign my_mipi_tx_VALID = prev_2_valid;
 assign my_mipi_tx_HSYNC = my_mipi_rx_HSYNC[0];
 assign my_mipi_tx_VSYNC = my_mipi_rx_VSYNC[0];
 assign my_mipi_tx_DATA = my_mipi_rx_DATA;
-assign my_mipi_tx_DPHY_RSTN = 1'b1;
-assign my_mipi_tx_RSTN = 1'b1;
+assign my_mipi_tx_DPHY_RSTN = 1;
+assign my_mipi_tx_RSTN = 1;
 assign my_mipi_tx_TYPE = 6'h24;			// RGB888
 assign my_mipi_tx_LANES = 2'b11;                // 4 lanes
 assign my_mipi_tx_FRAME_MODE = 1'b0;            // Generic Frame Mode
@@ -76,52 +63,18 @@ assign my_mipi_tx_ULPS_CLK_EXIT = 1'b0;
 //***************
 /* MIPI RX HOOKUP */
 //***************
-assign my_mipi_rx_DPHY_RSTN = 1'b1;
-assign my_mipi_rx_RSTN = 1'b1;
-assign my_mipi_rx_CLEAR = 1'b0;
-assign my_mipi_rx_LANES = 2'b01;         // 2 lanes
-assign my_mipi_rx_VC_ENA = 4'b0001;      // Virtual Channel enable
+assign my_mipi_rx_DPHY_RSTN = 1;
+assign my_mipi_rx_RSTN = 1;
+assign my_mipi_rx_CLEAR = 0;
+assign my_mipi_rx_LANES = 2'b11;         // 4 lanes
+assign my_mipi_rx_VC_ENA = 4'b0001;    // Virtual Channel enable
 
-assign led5 = my_mipi_rx_HSYNC[0];
-assign led6 = my_mipi_rx_VSYNC[0];
+assign led5 = my_mipi_rx_CLEAR;
+assign led6 = my_mipi_rx_RSTN;
+
+always @(posedge rx_pixel_clk) begin
+    prev_valid <= my_mipi_rx_VALID;
+    prev_2_valid <= prev_valid;
+end
 
 endmodule
-
-//////////////////////////////////////////////////////////////////////////////
-// Copyright (C) 2013-2019 Efinix Inc. All rights reserved.
-//
-// This   document  contains  proprietary information  which   is
-// protected by  copyright. All rights  are reserved.  This notice
-// refers to original work by Efinix, Inc. which may be derivitive
-// of other work distributed under license of the authors.  In the
-// case of derivative work, nothing in this notice overrides the
-// original author's license agreement.  Where applicable, the 
-// original license agreement is included in it's original 
-// unmodified form immediately below this header.
-//
-// WARRANTY DISCLAIMER.  
-//     THE  DESIGN, CODE, OR INFORMATION ARE PROVIDED “AS IS” AND 
-//     EFINIX MAKES NO WARRANTIES, EXPRESS OR IMPLIED WITH 
-//     RESPECT THERETO, AND EXPRESSLY DISCLAIMS ANY IMPLIED WARRANTIES, 
-//     INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF 
-//     MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR 
-//     PURPOSE.  SOME STATES DO NOT ALLOW EXCLUSIONS OF AN IMPLIED 
-//     WARRANTY, SO THIS DISCLAIMER MAY NOT APPLY TO LICENSEE.
-//
-// LIMITATION OF LIABILITY.  
-//     NOTWITHSTANDING ANYTHING TO THE CONTRARY, EXCEPT FOR BODILY 
-//     INJURY, EFINIX SHALL NOT BE LIABLE WITH RESPECT TO ANY SUBJECT 
-//     MATTER OF THIS AGREEMENT UNDER TORT, CONTRACT, STRICT LIABILITY 
-//     OR ANY OTHER LEGAL OR EQUITABLE THEORY (I) FOR ANY INDIRECT, 
-//     SPECIAL, INCIDENTAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES OF ANY 
-//     CHARACTER INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF 
-//     GOODWILL, DATA OR PROFIT, WORK STOPPAGE, OR COMPUTER FAILURE OR 
-//     MALFUNCTION, OR IN ANY EVENT (II) FOR ANY AMOUNT IN EXCESS, IN 
-//     THE AGGREGATE, OF THE FEE PAID BY LICENSEE TO EFINIX HEREUNDER 
-//     (OR, IF THE FEE HAS BEEN WAIVED, $100), EVEN IF EFINIX SHALL HAVE 
-//     BEEN INFORMED OF THE POSSIBILITY OF SUCH DAMAGES.  SOME STATES DO 
-//     NOT ALLOW THE EXCLUSION OR LIMITATION OF INCIDENTAL OR 
-//     CONSEQUENTIAL DAMAGES, SO THIS LIMITATION AND EXCLUSION MAY NOT 
-//     APPLY TO LICENSEE.
-//
-/////////////////////////////////////////////////////////////////////////////
